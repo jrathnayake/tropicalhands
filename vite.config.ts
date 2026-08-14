96f809e174005316b608104f6f3530f1591b2784
Joy Rathnayake <send2joy@gmail.com>
2026-08-14 23:04:12 +0530

    Trying to fix blank page

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,10 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
-const repoName = 'tropicalhands'
-
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? `/${repoName}/` : '/',
+export default defineConfig({
+  base: './',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
   ],
-}))
+})
